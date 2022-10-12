066382d308654b52585c6beeaf8941a90fee963c
Aubin Detrez <aubin.detrez@gmail.com>
2022-10-12 21:32:35 +0200

    CI: Add Sanity checks to verilog.sh
    Help new contributors with meaning full messages and make debugging CI
    faster.

--- ci/verilator.sh
+++ ci/verilator.sh
@@ -18,10 +18,24 @@ build_one_test () {
 	# Exhaustive test of 8 bit multiply/adder for every process and adder type
 	for PROCESS in ${PROCESSES}; do
 		for ADDER in ${ADDERS}; do
-			PIPELINE_CMD=$(echo $ARGS | sed -e 's/ --/-/g' -e 's/--//')
-			VERILOG="generated/multiplier_${PROCESS}_${ADDER}_${PIPELINE_CMD}_8.v"
-			BINARY=multiply-adder-${PROCESS}-${ADDER}_${PIPELINE_CMD}_8
-			vlsi-multiplier --bits=8 --tech=${PROCESS} --algorithm=${ADDER} --multiply-add ${ARGS} --output=${VERILOG}
+            # 's/\(.\)$/\1_/' will append _ at the end of the string (if not empty)
+			PIPELINE_CMD=$(echo $ARGS | sed -e 's/ --/-/g' -e 's/--//' -e 's/\(.\)$/\1_/')
+            WIDTH_BIT=8 # Size in bit
+			VERILOG="generated/multiplier_${PROCESS}_${ADDER}_${PIPELINE_CMD}${WIDTH_BIT}.v"
+			BINARY=multiply-adder-${PROCESS}-${ADDER}_${PIPELINE_CMD}_${WIDTH_BIT}
+            if ! [ -e $VERILOG ] ; then
+                echo "Error: Verilog File not found: $VERILOG"
+                exit 1
+            fi
+            # Generate verilog
+            # $ > vlsi-multiplier --bits=8 --multiply-add --algorithm=brentkung --tech=sky130hd  --output=generated/multiplier_sky130hd_b
+            #    vlsi-multiplier --bits=8 --tech=sky130hd --algorithm=brentkung --multiply-add  --output=generated/multiplier_sky130hd_bre
+            if ! [ -x vlsi-multiplier ] ; then
+                echo "Error: Cannot run vlsi-multiplier: Problem with python-venv or running from a different directory"
+                exit 1
+            fi
+			vlsi-multiplier --bits=${WIDTH_BIT} --tech=${PROCESS} --algorithm=${ADDER} --multiply-add ${ARGS} --output=${VERILOG}
+            # Compile test
 			verilator ${VERILATOR_OPTS} -CFLAGS "-O3 -DPIPELINE_DEPTH=${PIPELINE_DEPTH}" --assert --cc --exe --build ${VERILOG} verilog/${PROCESS}.v verilator/multiplier.cpp -o ${BINARY} -top-module multiply_adder
 			TESTS="${TESTS} obj_dir/${BINARY}"
 		done
