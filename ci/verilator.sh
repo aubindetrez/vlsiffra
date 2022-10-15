ecf100a3bde2f1ecb3a4e60ea8a65a8512f8e503
Aubin Detrez <aubin.detrez@gmail.com>
2022-10-15 10:37:22 +0200

    CI: Use relative path to the git repository's root
    Also contains:
    - Explicitely call python3 to interpret script (vlsi-multiplier)

--- ci/verilator.sh
+++ ci/verilator.sh
@@ -4,6 +4,8 @@ VERILATOR_OPTS="-O3 -Wno-fatal -Wno-TIMESCALEMOD"
 PROCESSES="sky130hd asap7 gf180mcu"
 ADDERS="brentkung koggestone hancarlson"
 
+gitroot="`git rev-parse --show-toplevel`"
+
 mkdir -p generated
 
 source venv/bin/activate
@@ -21,20 +23,18 @@ build_one_test () {
             # 's/\(.\)$/\1_/' will append _ at the end of the string (if not empty)
 			PIPELINE_CMD=$(echo $ARGS | sed -e 's/ --/-/g' -e 's/--//' -e 's/\(.\)$/\1_/')
             WIDTH_BIT=8 # Size in bit
-			VERILOG="generated/multiplier_${PROCESS}_${ADDER}_${PIPELINE_CMD}${WIDTH_BIT}.v"
+			VERILOG="${gitroot}/generated/multiplier_${PROCESS}_${ADDER}_${PIPELINE_CMD}${WIDTH_BIT}.v"
 			BINARY=multiply-adder-${PROCESS}-${ADDER}_${PIPELINE_CMD}_${WIDTH_BIT}
-            if ! [ -e $VERILOG ] ; then
-                echo "Error: Verilog File not found: $VERILOG"
+            if ! [ -e ${gitroot}/vlsiffra/vlsi-multiplier ] ; then
+                echo "Error: Cannot find vlsiffra/vlsi-multiplier (Searching the git repository: ${gitroot})"
                 exit 1
             fi
             # Generate verilog
-            # $ > vlsi-multiplier --bits=8 --multiply-add --algorithm=brentkung --tech=sky130hd  --output=generated/multiplier_sky130hd_b
-            #    vlsi-multiplier --bits=8 --tech=sky130hd --algorithm=brentkung --multiply-add  --output=generated/multiplier_sky130hd_bre
-            if ! [ -x vlsi-multiplier ] ; then
-                echo "Error: Cannot run vlsi-multiplier: Problem with python-venv or running from a different directory"
+			python3 ${gitroot}/vlsiffra/vlsi-multiplier --bits=${WIDTH_BIT} --tech=${PROCESS} --algorithm=${ADDER} --multiply-add ${ARGS} --output=${VERILOG}
+            if ! [ -e $VERILOG ] ; then
+                echo "Error: Verilog File not found (or failed to generate): $VERILOG"
                 exit 1
             fi
-			vlsi-multiplier --bits=${WIDTH_BIT} --tech=${PROCESS} --algorithm=${ADDER} --multiply-add ${ARGS} --output=${VERILOG}
             # Compile test
 			verilator ${VERILATOR_OPTS} -CFLAGS "-O3 -DPIPELINE_DEPTH=${PIPELINE_DEPTH}" --assert --cc --exe --build ${VERILOG} verilog/${PROCESS}.v verilator/multiplier.cpp -o ${BINARY} -top-module multiply_adder
 			TESTS="${TESTS} obj_dir/${BINARY}"
