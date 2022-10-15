c1ceb3d60c08341dc118ad13fbc10ee728b38790
Aubin Detrez <aubin.detrez@gmail.com>
2022-10-15 11:13:25 +0200

    CI: Formal: Use relative path to the git repository's root

--- ci/formal.sh
+++ ci/formal.sh
@@ -1,17 +1,19 @@
 #!/bin/bash -e
 
+PROCESSES="sky130hd asap7 gf180mcu"
+ADDERS="brentkung koggestone hancarlson"
+
+gitroot="`git rev-parse --show-toplevel`"
+
 mkdir -p generated
 
 source venv/bin/activate
 
-PROCESSES="sky130hd asap7 gf180mcu"
-ADDERS="brentkung koggestone hancarlson"
-
 # Test adders
 for PROCESS in ${PROCESSES}; do
 	for ADDER in ${ADDERS}; do
-		VERILOG=generated/adder_${PROCESS}_${ADDER}.v
-		vlsi-adder --bits=64 --algorithm=${ADDER} --tech=${PROCESS} --output=${VERILOG}
+		VERILOG=${gitroot}/generated/adder_${PROCESS}_${ADDER}.v
+		python3 ${gitroot}/vlsiffra/vlsi-adder --bits=64 --algorithm=${ADDER} --tech=${PROCESS} --output=${VERILOG}
 		BITS=64 VERILOG=${VERILOG} PROCESS_VERILOG=verilog/${PROCESS}.v yosys -c formal/adder.tcl
 	done
 done
@@ -19,8 +21,8 @@ done
 # Test multipliers
 for PROCESS in ${PROCESSES}; do
 	for ADDER in ${ADDERS}; do
-		VERILOG=generated/multiplier_${PROCESS}_${ADDER}.v
-		vlsi-multiplier --bits=8 --algorithm=${ADDER} --tech=${PROCESS} --output=${VERILOG}
+		VERILOG=${gitroot}/generated/multiplier_${PROCESS}_${ADDER}.v
+		python3 ${gitroot}/vlsiffra/vlsi-multiplier --bits=8 --algorithm=${ADDER} --tech=${PROCESS} --output=${VERILOG}
 		BITS=8 VERILOG=${VERILOG} PROCESS_VERILOG=verilog/${PROCESS}.v yosys -c formal/multiplier.tcl
 	done
 done
@@ -29,7 +31,7 @@ done
 for PROCESS in ${PROCESSES}; do
 	for ADDER in ${ADDERS}; do
 		VERILOG=generated/multiply_adder_${PROCESS}_${ADDER}.v
-		vlsi-multiplier --bits=4 --multiply-add --algorithm=${ADDER} --tech=${PROCESS} --output=${VERILOG}
+		python3 ${gitroot}/vlsiffra/vlsi-multiplier --bits=4 --multiply-add --algorithm=${ADDER} --tech=${PROCESS} --output=${VERILOG}
 		BITS=4 VERILOG=${VERILOG} PROCESS_VERILOG=verilog/${PROCESS}.v yosys -c formal/multiply_adder.tcl
 	done
 done
@@ -38,7 +40,7 @@ done
 for PROCESS in ${PROCESSES}; do
 	for ADDER in ${ADDERS}; do
 		VERILOG=generated/multiply_adder_${PROCESS}_${ADDER}_pipelined.v
-		vlsi-multiplier --bits=4 --multiply-add --algorithm=${ADDER} --tech=${PROCESS} --register-input --register-post-ppa --register-post-ppg --register-output --output=${VERILOG}
+		python3 ${gitroot}/vlsiffra/vlsi-multiplier --bits=4 --multiply-add --algorithm=${ADDER} --tech=${PROCESS} --register-input --register-post-ppa --register-post-ppg --register-output --output=${VERILOG}
 		BITS=4 VERILOG=${VERILOG} PROCESS_VERILOG=verilog/${PROCESS}.v yosys -c formal/multiply_adder_pipelined.tcl
 	done
 done
